3a812d286c163a711eef398d3913c10f71532a6b
Noah <volpy2000@gmail.com>
2025-02-24 11:41:40 +0100

    Added: Block

---
+++ app/src/main/java/com/noah/tictactoe/ui/comonents/Block.kt
@@ -0,0 +1,20 @@
+package com.noah.tictactoe.ui.comonents
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun Block(
+    padding: Dp = 30.dp,
+    content: @Composable () -> Unit
+) {
+    Column(
+        modifier = Modifier.padding(padding)
+    ) {
+        content()
+    }
+}
---
+++ app/src/main/java/com/noah/tictactoe/ui/comonents/Container.kt
@@ -0,0 +1,27 @@
+package com.noah.tictactoe.ui.comonents
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun Container(
+    content: @Composable() () -> Unit,
+) {
+    Column(
+        modifier = Modifier
+            .background(Color(0xFF202028))
+            .fillMaxSize(),
+
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        content()
+    }
+}
+
