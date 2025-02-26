034ddd3ed2d768198c09a115c5065a33c7073594
Noah <volpy2000@gmail.com>
2025-02-26 15:03:52 +0100

    Added

--- app/src/main/java/com/noah/tictactoe/data/PreferenceManager.kt
+++ app/src/main/java/com/noah/tictactoe/data/PreferenceManager.kt
@@ -1,4 +1,17 @@
 package com.noah.tictactoe.data
 
-class SharepreferenceManager {
+import android.content.Context
+import android.content.SharedPreferences
+
+class PreferenceManager(context: Context) {
+    private val sharedPreferences: SharedPreferences =
+        context.getSharedPreferences("preferences", Context.MODE_PRIVATE)
+
+    fun saveString(key: String, value: String) {
+        sharedPreferences.edit().putString(key, value).apply()
+    }
+
+    fun getString(key: String, value: String? = null): String? {
+        return sharedPreferences.getString(key, value)
+    }
 }
