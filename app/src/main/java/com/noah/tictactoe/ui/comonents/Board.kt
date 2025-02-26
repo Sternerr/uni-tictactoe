2a76569d0f7e336f16a5af7f7b02b729d30e6e46
Noah <volpy2000@gmail.com>
2025-02-26 11:33:31 +0100

    Added: Board

--- app/src/main/java/com/noah/tictactoe/ui/comonents/Board.kt
+++ app/src/main/java/com/noah/tictactoe/ui/comonents/Board.kt
@@ -8,13 +8,20 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.noah.tictactoe.GameViewModel
+import com.noah.tictactoe.PlayerViewModel
 
 @Composable
-fun Board() {
+fun Board(playerViewModel: PlayerViewModel, gameViewModel: GameViewModel, gameID: String) {
+    val playerMap = playerViewModel.playerMap.collectAsStateWithLifecycle()
+    val gameMap = gameViewModel.gameMap.collectAsStateWithLifecycle()
+
     Column(
         modifier = Modifier
             .height(350.dp)
@@ -26,8 +33,28 @@ fun Board() {
             Spacer(Modifier.weight(1f))
             Row {
                 for (x in 0..2) {
+                    val coordinates = (y * 3 + x)
+                    val mark = when {
+                        gameMap.value[gameID]!!.board[coordinates] == 0 -> "X"
+                        gameMap.value[gameID]!!.board[coordinates] == 1 -> "O"
+                        else -> " "
+                    }
+
                     Spacer(Modifier.weight(1f))
-                    Cell("O") {}
+                    Cell(mark) {
+                        if((playerViewModel.localPlayerId == gameMap.value[gameID]!!.players[gameMap.value[gameID]!!.turn])
+                            && gameMap.value[gameID]!!.board[coordinates] == 2) {
+                            val newBoard = gameMap.value[gameID]!!.board.toMutableList()
+                            val turn = gameMap.value[gameID]!!.turn
+                            newBoard[coordinates] = turn
+
+                            gameViewModel.makeMove(
+                                gameID = gameID,
+                                turn = turn,
+                                newBoard = newBoard
+                            )
+                        }
+                    }
                 }
                 Spacer(Modifier.weight(1f))
             }
