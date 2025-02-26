01ce6443cb3fdce5b6eb1c662b8a2e12651c2777
Noah <volpy2000@gmail.com>
2025-02-26 11:38:48 +0100

    Added: Navigation

--- app/src/main/java/com/noah/tictactoe/Application.kt
+++ app/src/main/java/com/noah/tictactoe/Application.kt
@@ -1,2 +1,43 @@
 package com.noah.tictactoe
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.noah.tictactoe.ui.screens.GameScreen
+import com.noah.tictactoe.ui.screens.LobbyScreen
+import com.noah.tictactoe.ui.screens.LoginScreen
+import com.noah.tictactoe.ui.screens.ResultScreen
+
+@Composable
+fun Application() {
+    val navController = rememberNavController()
+    val playerViewModel = PlayerViewModel()
+    val gameViewModel = GameViewModel()
+
+    NavHost(navController, startDestination = "join") {
+        composable(route = "join") {
+            LoginScreen(navController, playerViewModel)
+        }
+
+        composable(route = "lobby") {
+            LobbyScreen(navController, playerViewModel, gameViewModel)
+        }
+
+        composable(route = "game/{gameID}") { backStackEntry ->
+            val gameID = backStackEntry.arguments?.getString("gameID")
+
+            if (gameID != null) {
+                GameScreen(navController, playerViewModel, gameViewModel, gameID)
+            }
+        }
+
+        composable(route = "result/{gameID}") { navBackStackEntry ->
+            val gameID = navBackStackEntry.arguments?.getString("gameID")
+
+            if (gameID != null) {
+                ResultScreen(navController, playerViewModel, gameViewModel, gameID)
+            }
+        }
+    }
+}
--- app/src/main/java/com/noah/tictactoe/MainActivity.kt
+++ app/src/main/java/com/noah/tictactoe/MainActivity.kt
@@ -1,40 +1,24 @@
 package com.noah.tictactoe
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.core.view.KeyEventDispatcher.Component
+import androidx.annotation.RequiresApi
+
 import com.noah.tictactoe.ui.theme.TicTacToeTheme
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.Q)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
         setContent {
             TicTacToeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
-                }
+                Application()
             }
         }
     }
 }
-
-
